fa5eee96408d2a55e87d19b27f178469f78c094c
NeverNotJohn <johntheherrera@gmail.com>
2023-08-05 17:40:15 -0400

    made template

---
+++ copy_me/template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+    
+int func_name(vector<string>& input) {
+    // Code here
+}
+
+
+int main() 
+{
+    // Test cases
+    vector<string> input = {"abc", "b", "c", "wsaf", "fuck"};
+    
+    func_name(input);
+    
+    return 0;
+}
