967bb4dd66b34c72dad3be3ccef5cde2ac77c876
NeverNotJohn <johntheherrera@gmail.com>
2023-08-06 00:04:30 -0400

    yay i think i got it

--- productExceptSelf/main.cpp
+++ productExceptSelf/main.cpp
@@ -5,27 +5,37 @@
 using namespace std;
     
 vector<int> productExceptSelf(vector<int>& nums) {
-        
-        vector<int> ans;
-        vector<int> minor;
-        vector<int> major;
+    
+    vector<int> ans(nums.size(), 0);
+    vector<int> minor(nums.size(), 0);
+    vector<int> major(nums.size(), 0);
 
-        minor[0] = nums[0]; // product of everything to the left
-        major[major.size() - 1] = nums[0]; // product of everything to the right 
+    minor[0] = nums[0]; // product of everything to the left
+    major[major.size() - 1] = nums[major.size() - 1]; // product of everything to the right 
 
-        for (int i = 1; i < nums.size(); i++) // loop thru nums... create minor
-        {   
-            minor[i] = minor[i-1] * nums[i];
-        }
-
-        for (int j = major.size() - 2; j >= 0; j++)
-        {
-            major[j] = major[j + 1] * nums[j];
-        }
-
-        return ans;
-
+    for (int i = 1; i < nums.size(); i++) // loop thru nums... create minor
+    {   
+        minor[i] = minor[i-1] * nums[i];
     }
+
+    for (int j = major.size() - 2; j >= 0; j--) // create major
+    {
+        major[j] = major[j + 1] * nums[j];
+    }
+
+    // calculate answer
+
+    ans[0] = major[1];
+    ans[nums.size() - 1] = minor[minor.size() - 2];  
+
+    for (int i=1; i < nums.size()-1; i++)
+    {
+        ans[i] = minor[i-1] * major[i+1];
+    }
+
+    return ans;
+
+}
 
 int main() 
 {
