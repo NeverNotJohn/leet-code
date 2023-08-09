4f467c1c860459590bd5aed2ca420cdf3d65c1b3
NeverNotJohn <johntheherrera@gmail.com>
2023-08-09 00:08:08 -0400

    ez

---
+++ isPalindrome/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+    
+bool isPalindrome(string s) {
+        
+        string new_s = "";
+
+        // remove stuff from string
+
+        for (char c : s) 
+        {
+            if (isalnum(c)) {
+                new_s += tolower(c);
+            }
+        }
+
+        if (new_s == "") { return true; }
+
+        for (int i = 0; i < new_s.size(); i++)
+        {
+            char p1 = new_s[i];
+            char p2 = new_s[new_s.size()-(1 + i)];
+            
+            if (p1 != p2) { return false; }
+
+        }
+
+        return true;
+
+}
+
+
+int main() 
+{
+    // Test cases
+    string input = "Hell oworld!";
+    
+    isPalindrome(input);
+    
+    return 0;
+}
